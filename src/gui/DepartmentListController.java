064c2fc6677f10f54b03dd104d23c798cad19668
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2022-02-16 23:12:26 -0300

    salvando um novo departamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -85,6 +85,7 @@ public class DepartmentListController implements Initializable{
 			
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormatData();
 			
 			Stage dialogStage = new Stage();
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -13,5 +13,14 @@ public class DepartmentService {
 	public List<Department> findAll(){
 		return dao.findAll();
 	}
+	
+	public void SaveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 
 }
